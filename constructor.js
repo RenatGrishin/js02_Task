b845f31c9a9f4e80fb41b5d15fe6c2db417bdba0
Renat Grishin <53834553+RenatGrishin@users.noreply.github.com>
2020-08-23 21:40:20 +0300

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,81 @@
+let view = {
+    creatTaskUI:function(taskDIV, taskInfo){
+        taskDIV.insertAdjacentHTML("afterbegin", `<div class="taskLine" id="${taskInfo.id}">
+            <input class="deleteTask" onclick="controller.deleteTaskTable(${taskInfo.id})" type="submit" value="X">
+            <p onclick="controller.editTaskTable(${taskInfo.id})">${taskInfo.task}</p>
+        </div>`);
+    },
+    showTasks: function(allTasks){
+        let taskList = document.querySelector('#taskArea');
+        for(let i=0; i<allTasks.length; i++){
+            this.creatTaskUI(taskList, allTasks[i]);
+        }
+    },
+    addTaskInList: function(taskInfo){
+        let taskList = document.querySelector('#taskArea');
+        this.creatTaskUI(taskList, taskInfo);
+    },
+    deleteTaskInList: function(idTask){
+        let taskList = document.getElementById(idTask).remove();
+    },
+    editTask: function(idTask, taskInfo){
+        let taskList = document.getElementById(idTask);
+        while(taskList.firstChild){
+            taskList.removeChild(taskList.firstChild);
+        }
+        taskList.insertAdjacentHTML("afterbegin",`<textarea id="texta">${taskInfo.task}</textarea>`);
+        document.getElementById('texta').focus();
+        
+        texta.onblur = function(){
+            let textareaInf = {
+                id: taskInfo.id, 
+                task: texta.value
+            }
+            document.getElementById('texta').remove();
+            taskList.insertAdjacentHTML("afterbegin", `<input class="deleteTask" onclick="controller.deleteTaskTable(${textareaInf.id})" type="submit" value="X">
+            <p onclick="controller.editTaskTable(${textareaInf.id})">${textareaInf.task}</p>`)
+        }
+    }
+}
+
+let model = {
+    taskList:[
+        {
+            id: 0,
+            task: 'Первая тестовая запись'
+        }
+    ],
+    addTask: function(text, taskList){
+        return {
+            id: taskList.length, 
+            task: text
+        }
+    },
+    deleteTask: function(id){
+        delete this.taskList[id];
+    },
+    editTask: function(taskId){
+        return this.taskList[taskId];
+    }
+}
+
+let controller = {
+    showTaskTable: function(){
+        view.showTasks(model.taskList);
+    },
+    addTaskTable: function(addText){
+        let newTask = model.addTask(addText, model.taskList);
+        model.taskList.push(newTask);
+        view.addTaskInList(newTask);
+    },
+    deleteTaskTable: function(idTask){
+        model.deleteTask(idTask);
+        view.deleteTaskInList(idTask);
+    },
+    editTaskTable: function(idTask){
+        let taskInfo = model.editTask(idTask);
+        console.log(view.editTask(idTask, taskInfo))
+    }
+}
+
+controller.showTaskTable();
